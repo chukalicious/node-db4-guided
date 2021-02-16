160a5d673888c10f9fc076b1d915de220343cb3f
Katiuska Alicea <katiuskaalicea@gmail.com>
2021-02-16 15:33:39 -0800

    tables are created, migrations and seeds ran, created a zoos endpoint, which was missing

--- api/server.js
+++ api/server.js
@@ -1,69 +1,75 @@
-const express = require('express');
-const helmet = require('helmet');
+const express = require("express");
+const helmet = require("helmet");
 
-const db = require('../data/db-config.js');
+const db = require("../data/db-config.js");
 
 const server = express();
 
 server.use(helmet());
 server.use(express.json());
 
-server.get('/api/species', (req, res) => {
+server.get("/api/species", (req, res) => {
   // get all species from the database
-  db('species')
-    .then(species => {
+  db("species")
+    .then((species) => {
       res.status(200).json(species);
     })
-    .catch(error => {
+    .catch((error) => {
       res.status(500).json(error);
     });
 });
 
-server.get('/api/animals', (req, res) => {
+server.get("/api/zoos", async (req, res) => {
+  const zoos = await db("zoos");
+  res.status(200).json(zoos);
+});
+
+server.get("/api/animals", (req, res) => {
   // get all animals from the database
   // include species name
-  db('animals as a')
-    .leftJoin('species as s', 's.id', 'a.species_id')
-    .select('a.id', 'a.animal_name', 's.species_name')
-    .then(animals => {
+  db("animals as a")
+    .leftJoin("species as s", "s.id", "a.species_id")
+    .select("a.id", "a.animal_name", "s.species_name")
+    .then((animals) => {
       res.status(200).json(animals);
     })
-    .catch(error => {
+    .catch((error) => {
       res.status(500).json(error);
     });
 });
 
 // create animal
-server.post('/api/animals', (req, res) => {
-  db('animals').insert(req.body)
-    .then(ids => {
+server.post("/api/animals", (req, res) => {
+  db("animals")
+    .insert(req.body)
+    .then((ids) => {
       const id = ids[0];
 
-      db('animals')
+      db("animals")
         .where({ id })
         .first()
-        .then(animal => {
+        .then((animal) => {
           res.status(201).json(animal);
         });
     })
-    .catch(error => {
+    .catch((error) => {
       res.status(500).json(error);
     });
 });
 
 // remove species
-server.delete('/api/species/:id', (req, res) => {
-  db('species')
+server.delete("/api/species/:id", (req, res) => {
+  db("species")
     .where({ id: req.params.id })
     .del()
-    .then(count => {
+    .then((count) => {
       if (count > 0) {
         res.status(204).end();
       } else {
-        res.status(404).json({ message: 'Record not found' });
+        res.status(404).json({ message: "Record not found" });
       }
     })
-    .catch(error => {
+    .catch((error) => {
       res.status(500).json(error);
     });
 });
--- data/migrations/20210216142916_initial.js
+++ data/migrations/20210216142916_initial.js
@@ -20,11 +20,31 @@ exports.up = function (knex) {
       tbl.text("animal_name").notNullable();
       //the line below creates a foreign key
       tbl.integer("species_id").references("id").inTable("species");
+    })
+    .createTable("zoos_animals", (tbl) => {
+      tbl.integer("zoo_id").references("id").inTable("zoos").notNullable();
+      tbl
+        .integer("animal_id")
+        .references("id")
+        .inTable("animals")
+        .notNullable();
+      //knex.raw will pass 'current_timestamp' without quotes,
+      //meaning it's an internal SQL variable and not a
+      //literal string
+      tbl
+        .date("from_date")
+        .defaultTo(knex.raw("current_timestamp"))
+        .notNullable();
+      tbl.date("to_date");
+      //since this table doesn't need an ID column, we can make the primary
+      //key a combination of two columns rather than a single one
+      tbl.primary(["zoo_id", "animal_id"]);
     });
 };
 
 exports.down = function (knex) {
   return knex.schema
+    .dropTableIfExists("zoos_animals")
     .dropTableIfExists("animals")
     .dropTableIfExists("species")
     .dropTableIfExists("zoos");
