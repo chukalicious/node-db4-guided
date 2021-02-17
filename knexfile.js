bb4b33e4cb3623153e3e2782151f8c962343dbd9
Katiuska Alicea <katiuskaalicea@gmail.com>
2021-02-16 18:48:45 -0800

    fixed bug on pool onject in knexfile.js

--- knexfile.js
+++ knexfile.js
@@ -5,6 +5,12 @@ module.exports = {
     connection: {
       filename: "./data/zoos.db3",
     },
+    pool: {
+      afterCreate: (conn, done) => {
+        conn.run("PRAGMA foreign_keys = ON", done);
+      },
+    },
+
     migrations: {
       directory: "./data/migrations",
     },
